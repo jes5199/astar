1bdcc2158ce15460b0e1058a2b447cc1e487acf3
Jesse Wolfe <jes5199@gmail.com>
2011-03-28 11:52:22 -0700

    comments and some simplifications

--- astar.rb
+++ astar.rb
@@ -1,27 +1,33 @@
+require 'set'
+class AStar # subclass me!
+  protected
 
-class AStar
+  # override neighbors, edge_cost, and heuristic for your problem domain.
   def neighbors( node )
-    []
+    [] # should return a list of nodes (or any enumerable, really)
   end
 
-  def edge_cost( from, to )
+  def edge_cost( from, to ) # real cost between nodes
     1
   end
 
-  def heuristic( from, to )
+  def heuristic( from ) # estimated cost to goal
     0
-  end
-
-  def is_goal?( node )
-    goal === node
   end
 
   def start_cost
-    0
+    0 # If you want to use something other than integers, you may need to override start_cost
   end
 
   def visited( node )
     # noop, override as a hook
+  end
+
+
+  # Convenience methods. Usually you won't need to override them.
+
+  def is_goal?( node )
+    goal === node
   end
 
   def known_cost( node )
@@ -29,66 +35,68 @@ class AStar
   end
 
   def estimated_cost( current )
-    @known_cost[current] + heuristic( current, @goal )
+    @known_cost[current] + heuristic( current )
   end
 
   attr_reader :start, :goal
   def initialize( start, goal )
     @start, @goal = start, goal
-    @open = {start => true}
-    @parent = {}
-    @closed = {}
+    @open = Set.new([start])
+    @parent = Hash.new
+    @closed = Set.new
     @known_cost = {start => start_cost}
   end
 
+  public
+
+  def search # The actual algorithm
+    loop do
+      node = find_next
+
+      return nil if node.nil?
+
+      visited(node)
+
+      return path_to(node) if is_goal?(node)
+
+      close( node )
+      search_neighbors( node )
+    end
+  end
+
+  private
+
   def close( node )
-    @closed[node] = true
+    @closed << node
     @open.delete(node)
   end
 
-  def check_neighbors( node )
-    neighbors(node).each do |n|
-      next if @closed[n]
-      direct_cost = known_cost(node) + edge_cost( node , n )
-      if ! known_cost(n) or known_cost(n) > direct_cost
-        @known_cost[n] = direct_cost
-        @parent[n] = node
-        @open[n] = true
+  def maybe_assign_parent( parent, child )
+      child_cost = known_cost(parent) + edge_cost( parent , child )
+
+      if ! known_cost(child) or known_cost(child) > child_cost # This path seems better than the best known to that node
+        @known_cost[child] = child_cost
+        @parent[child] = parent
+        @open << child
       end
+  end
+
+  def search_neighbors( node )
+    neighbors(node).each do |neighbor|
+      next if @closed.include?(neighbor)
+      maybe_assign_parent( node, neighbor )
     end
   end
 
   def find_next
-    @open.keys.sort_by{ |n| estimated_cost( n ) }.first
+    @open.sort_by{ |n| estimated_cost( n ) }.first
   end
 
   def path_to(node)
     path = []
-    while @parent[node] do
+    while node do
       path = [node] + path
       node = @parent[node]
-    end
-  end
-
-  def search(&blk)
-    loop do
-      node = find_next
-
-      visited(node)
-
-      return nil if node.nil?
-
-      if is_goal?(node)
-        path = path_to(node)
-        if blk
-          blk.call( path )
-        else
-          return path
-        end
-      end
-
-      close( node )
-      check_neighbors( node )
     end
   end
 
